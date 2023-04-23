06fa76d6ddc8eeeffade7b01188d1cca9cfc95d2
collin-mccarter <collinmccarter12@gmail.com>
2023-04-22 21:18:24 -0500

    npm i

---
+++ Develop/server.js
@@ -0,0 +1,65 @@
+// Setting up dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+// Setting up Express
+const app = express()
+const PORT = prpcess.env.PORT || 3001
+
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+app.use(express.static("public"))
+
+// Start server
+app.listen(PORT, () => 
+console.log(`Notes App Is Live at ${PORT}`))
+
+// Creating a API GET
+const notes = require("./db/db.json")
+app.get("/api/notes", (req,res) => res.json(notes))
+
+// API POST
+app.post("/api/notes", (req,res) => {
+    notes.push(req.body)
+    
+    let id = 1
+    
+    notes.forEach((notes) => {
+        notes.id = id
+        id++
+        return notes
+    })
+
+    res.json(true);
+
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+        if(err) throw err
+    })
+
+    res.end()
+})
+
+app.delete("/api/notes/:id", function (req,res) {
+    const deleteNote = req.params.id
+    for(let i = 0; i < notes.length; i++) {
+        if (notes[i].id === Number(deleteNote)) {
+            notes.splice(i,1);
+        }
+
+        fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+            if(err) throw err;
+        })
+        res.end()
+    }
+})
+
+// HTML GET
+app.get("./notes", (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+
+// Default to home
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+})
